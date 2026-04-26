4c32593964a8c7f3919efdedd568e081b23276ee
PauMerr95 <pp.retail@posteo.net>
2026-04-26 19:22:39 +0200

    feat(leader.ts): New Leader Icon for commandline

--- src/app/components/icons/leader.ts
+++ src/app/components/icons/leader.ts
@@ -5,9 +5,9 @@ import { Color } from '../../types/main_types';
   selector: 'app-icon-leader',
   imports: [],
   template: `
-    <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" [attr.color]="svg_color">
-      <path d="M8 7L3 12L8 17" [attr.stroke]="svg_color" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-      <path d="M16 7L21 12L16 17" [attr.stroke]="svg_color" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+    <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" [attr.stroke]="svg_color">
+      <path d="M21 3.6V20.4C21 20.7314 20.7314 21 20.4 21H3.6C3.26863 21 3 20.7314 3 20.4V3.6C3 3.26863 3.26863 3 3.6 3H20.4C20.7314 3 21 3.26863 21 3.6Z" [attr.stroke]="svg_color" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+      <path d="M15.0251 8.02513H10.0754M15.0251 8.02513V12.9749M15.0251 8.02513C15.0251 8.02513 12.1967 10.8536 11.4896 11.5607C9.01472 14.0355 11.4896 16.5104 11.4896 16.5104" [attr.stroke]="svg_color" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
     </svg>
   `,
 })
